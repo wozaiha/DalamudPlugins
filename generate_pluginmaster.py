cc2226b6a4242a3ffe0b240c32c319965be02777
Tsubasahane <wozaiha@gmail.com>
2023-02-18 23:47:10 +0800

    Update generate_pluginmaster.py

--- generate_pluginmaster.py
+++ generate_pluginmaster.py
@@ -6,7 +6,7 @@ from sys import argv
 from os.path import getmtime
 from zipfile import ZipFile, ZIP_DEFLATED
 
-DOWNLOAD_URL = 'https://raw.githubusercontent.com/wozaiha/DalamudPlugins/CustomRepo/plugins/{plugin_name}/latest.zip'
+DOWNLOAD_URL = 'https://dalamudplugins-1253720819.cos.ap-nanjing.myqcloud.com/plugins/{plugin_name}/latest.zip'
 
 DEFAULTS = {
     'IsHide': False,
@@ -47,13 +47,13 @@ def main():
     last_updated()
 
     # update the Markdown
-    #update_md(master)
+    update_md(master)
 
 def extract_manifests():
     manifests = []
 
     for dirpath, dirnames, filenames in os.walk('./plugins'):
-        if len(filenames) == 0 or (dirpath.split('/')[-1].split('\\')[-1]+'.zip') not in filenames:
+        if len(filenames) == 0 or 'latest.zip' not in filenames:
             continue
         plugin_name = dirpath.split('/')[-1].split('\\')[-1]
         latest_json = f'{dirpath}/{plugin_name}.json'
@@ -63,6 +63,20 @@ def extract_manifests():
                 content = content.encode('utf8')[3:].decode('utf8')
             manifests.append(json.loads(content))
 
+    translations = {}
+    with codecs.open("translations.json", "r", "utf-8") as f:
+        translations = json.load(f)
+        for manifest in manifests:
+            desc = manifest.get('Description')
+            if desc and desc not in translations:
+                translations[desc] = ""
+    with codecs.open("translations.json", "w", "utf-8") as f:
+        json.dump(translations, f, indent=4)
+
+    for manifest in manifests:
+        desc = manifest.get('Description')
+        if desc in translations:
+            manifest['Description'] = translations[desc]
     return manifests
 
 def add_extra_fields(manifests):
@@ -93,7 +107,7 @@ def last_updated():
         master = json.load(f)
 
     for plugin in master:
-        latest = f'plugins/{plugin["InternalName"]}/{plugin["InternalName"]}.zip'
+        latest = f'plugins/{plugin["InternalName"]}/latest.zip'
         modified = int(getmtime(latest))
 
         if 'LastUpdated' not in plugin or modified != int(plugin['LastUpdated']):
